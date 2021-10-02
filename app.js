c56f0b7364eda29166d530d189815d498572ba45
marrerap <andrew.marrero@outlook.com>
2021-10-02 12:21:42 -0500

    created rotues for home and new, created views for home and new added a partials folder

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ const port = 3000
 
 
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var homeRouter = require('./routes/home');
+var newRouter = require('./routes/new');
 
 
 var app = express();
@@ -29,11 +29,10 @@ app.use(cookieParser());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', homeRouter);
+app.use('/new', newRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../model/trip')
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('home', { title: 'Trip Planner' });
 });
 
 module.exports = router;
