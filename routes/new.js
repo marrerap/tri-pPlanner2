1a1ea0483e63ce3f2844e988f6ea5ba2ffc2a020
marrerap <andrew.marrero@outlook.com>
2021-10-03 09:11:27 -0500

    added post route for new trips

--- routes/new.js
+++ routes/new.js
@@ -1,10 +1,26 @@
+const { application } = require('express');
 var express = require('express');
 var router = express.Router();
 const db = require('../model/trip')
+
+let id = 3
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.render('new', { title: 'New Trip' });
 });
+
+app.post('/', function(req,res) {
+  const newTrip = {
+    id = id++,
+    title = req.body.trip_title.toLowerCase().replace(" ", "_"),
+    departure_date = req.body.departure_date,
+    return_date = req.body.return_date
+  }
+  db.push(newTrip)
+  console.log('New trip received', newTrip)
+  res.redirect('/')
+})
+
 
 module.exports = router;
